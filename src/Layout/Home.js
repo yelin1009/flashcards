758674a03e8a3faf9f1a6069e445e1b9be4d2945
Yelin <yelin.lee.1009@gmail.com>
2021-04-21 10:04:02 -0700

    completed flashcards

---
+++ src/Layout/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import DeckList from "../Deck/DeckList.js";
+import { Link } from "react-router-dom";
+
+function Home() {
+  return (
+    <div>
+      <DeckList />
+    </div>
+  );
+}
+
+export default Home;
